507264b3904910462a8ae17194ed4dc937c4dab8
Darby Lim <thlim@robotis.com>
2020-03-21 17:13:31 +0900

    Added new motions

--- LA PORTE DES SENS/crank_xl320/crank_xl320.ino
+++ LA PORTE DES SENS/crank_xl320/crank_xl320.ino
@@ -26,9 +26,7 @@
 
 #define BAUDRATE  1000000
 #define DXL_CNT 10
-
-const int analogInPin = 0;
-int sonic_data;
+uint8_t dxl_id[DXL_CNT] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
 DynamixelWorkbench dxl_wb; 
 
@@ -38,8 +36,16 @@ int32_t moving_speed[2] = {512, 512};
 const uint8_t goal_position_handler = 0;
 const uint8_t moving_speed_handler = 1;
 
-#define DOWN 512
-#define UP 0
+const int analogInPin = 0;
+int sonic_data;
+
+int led_pin = 14;
+
+#define OPENED 0
+#define CLOSED 512
+
+#define FAST 1023
+#define SLOW 100
 
 void setup() 
 {
@@ -47,13 +53,12 @@ void setup()
 //  while(!Serial); // Wait for Opening Serial Monitor
 
   pinMode(analogInPin, INPUT_ANALOG);
+  pinMode(led_pin, OUTPUT);
 
   const char *log;
   bool result = false;
 
   uint16_t model_number = 0;
-  uint8_t dxl_id[DXL_CNT] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
   result = dxl_wb.init(DEVICE_NAME, BAUDRATE, &log);
   if (result == false)
   {
@@ -66,7 +71,7 @@ void setup()
     Serial.println(BAUDRATE);  
   }
 
-  for (int cnt = 0; cnt <= DXL_CNT; cnt++)
+  for (int cnt = 0; cnt < DXL_CNT; cnt++)
   {
     result = dxl_wb.ping(dxl_id[cnt], &model_number, &log);
     if (result == false)
@@ -81,6 +86,7 @@ void setup()
       Serial.print(dxl_id[cnt]);
       Serial.print(" model_number : ");
       Serial.println(model_number);
+      digitalWrite(led_pin, HIGH);
     }
 
     result = dxl_wb.jointMode(dxl_id[cnt], 0, 0, &log);
@@ -92,7 +98,9 @@ void setup()
     else
     {
       Serial.println("Succeed to change joint mode");
+      digitalWrite(led_pin, HIGH);
     }
+    delay(50);
   } 
 
   randomSeed(analogRead(1));
@@ -100,103 +108,156 @@ void setup()
 
 void fast()
 {
-  speed(1023);
-  move(1000, DOWN);
-
-  speed(1023);
-  move(1000, UP);
+  move(FAST, OPENED, 1000);
+  move(FAST, CLOSED, 1000);
 }
 
 void slow()
 {
-  speed(100);
-  move(3000, DOWN);
-
-  speed(100);
-  move(3000, UP);
+  move(SLOW, OPENED, 3000);
+  move(SLOW, CLOSED, 3000);
 }
 
 void fast_slow()
 {
-  speed(1023);
-  move(1000, DOWN);
+  move(FAST, OPENED, 1000);
+  move(SLOW, CLOSED, 3000);
+}
 
-  speed(100);
-  move(3000, UP);
+void slow_fast()
+{
+  move(SLOW, OPENED, 1000);
+  move(FAST, CLOSED, 3000);
 }
 
 void swing()
 {
   for (int i = 0; i < 10; i++)
   {
-    speed(1023);
-    move(1, DOWN);
-  
-    speed(1023);
-    move(1, UP);
+    move(FAST, OPENED, 10);
+    move(FAST, CLOSED, 10);
   }
 
-  speed(100);
-  move(3000, DOWN);
+  slow();
+}
+
+void stair_slow()
+{
+  for (int cnt = 0; cnt < DXL_CNT; cnt++)
+  {
+    move(dxl_id[cnt], SLOW, OPENED, 1000);
+  }
+
+  for (int cnt = 0; cnt < DXL_CNT; cnt++)
+  {
+    move(dxl_id[cnt], SLOW, CLOSED, 1000);
+  }
+}
+
+void stair_fast()
+{
+  for (int cnt = 0; cnt < DXL_CNT; cnt++)
+  {
+    move(dxl_id[cnt], FAST, OPENED, 100);
+  }
+
+  for (int cnt = 0; cnt < DXL_CNT; cnt++)
+  {
+    move(dxl_id[cnt], FAST, CLOSED, 100);
+  }
 }
 
 void loop() 
-{  
-  sonic_data = analogRead(analogInPin) * 3;
-
-  if (sonic_data < 1000)
+{ 
+  const int MOTION_NUM = 7;
+  const float CONTROL_PERIOD = 0.050; // sec
+  
+  static uint32_t t = millis();
+  if ((millis() - t) >= (CONTROL_PERIOD * 1000))
   {
-    long rand_num = random(1000);
-    if (rand_num % 4 == 0)
+    sonic_data = analogRead(analogInPin) * 3;
+  
+    if (sonic_data < 1000)
     {
-      fast();
+      long rand_num = random(1000);
+      if (rand_num % MOTION_NUM == 0)
+      {
+        fast();
+      }
+      else if (rand_num % MOTION_NUM == 1)
+      {
+        slow();
+      }
+      else if (rand_num % MOTION_NUM == 2)
+      {
+        fast_slow();
+      }
+      else if (rand_num % MOTION_NUM == 3)
+      {
+        slow_fast();
+      }
+      else if (rand_num % MOTION_NUM == 4)
+      {
+        swing();
+      }
+      else if (rand_num % MOTION_NUM == 5)
+      {
+        stair_fast();
+      }
+      else if (rand_num % MOTION_NUM == 6)
+      {
+        stair_slow();
+      }
     }
-    else if (rand_num % 4 == 1)
-    {
-      slow();
-    }
-    else if (rand_num % 4 == 2)
-    {
-      fast_slow();
-    }
-    else if (rand_num % 4 == 3)
-    {
-      swing();
-    }
+
+    t = millis();
   }
 }
 
-void set_position(int32_t a)
+void set_position(int32_t pos)
 {
-  if (a > 512) a = 512;
-  if (a < 0) a = 0;
+  if (pos > 512) pos = 512;
+  if (pos < 0) pos = 0;
   
-  goal_position[0] = a;
+  goal_position[0] = pos;
 }
 
-void set_speed(int32_t a)
+void set_speed(int32_t spd)
 {
-  if (a > 1023) a = 1023;
-  if (a < 0) a = 0;
+  if (spd > 1023) spd = 1023;
+  if (spd < 0) spd = 0;
   
-  moving_speed[0] = a;
+  moving_speed[0] = spd;
 }
 
-void move(uint32_t move_time, int32_t a)
+void move(int32_t spd, int32_t pos, uint32_t wait)
 {
-  set_position(a);
+  speed(spd);
+  
+  set_position(pos);
 
   for (int id = 0; id <= DXL_CNT; id++)
   {
     dxl_wb.goalPosition(id, goal_position[0]);
   }
 
-  delay(move_time);
+  delay(wait);
 }
 
-void speed(int32_t a)
+void move(int32_t id, int32_t spd, int32_t pos, uint32_t wait)
 {
-  set_speed(a);
+  speed(spd);
+  
+  set_position(pos);
+
+  dxl_wb.goalPosition(id, goal_position[0]);
+
+  delay(wait);
+}
+
+void speed(int32_t spd)
+{
+  set_speed(spd);
   
   for (int id = 0; id <= DXL_CNT; id++)
   {
