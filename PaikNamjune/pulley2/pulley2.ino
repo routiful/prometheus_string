a35641bc78280e9956577e645cc565e16914565e
Darby Lim <thlim@robotis.com>
2020-09-19 16:23:35 +0900

    Added motors

--- PaikNamjune/pulley2/pulley2.ino
+++ PaikNamjune/pulley2/pulley2.ino
@@ -1,5 +1,4 @@
 #include <DynamixelShield.h>
-#include <SoftwareSerial.h>
 
 //#if defined(ARDUINO_AVR_UNO) || defined(ARDUINO_AVR_MEGA2560)
 //  #include <SoftwareSerial.h>
@@ -11,9 +10,11 @@
 //  #define DEBUG_SERIAL Serial
 //#endif
 
-#define FIRST_DXL 1
-#define SECOND_DXL  2
-#define THIRD_DXL  3
+//#define FIRST_DXL 1
+//#define SECOND_DXL  2
+//#define THIRD_DXL  3
+
+#define DXL_CNT 15
 
 const float UP = 1.0; // CW
 const float DOWN = -1.0; // CCW
@@ -22,130 +23,43 @@ const float DXL_ONE_ROTATION = 4096.0;
 const float DXL_HALF_ROTATION = 2048.0;
 const float DXL_ZERO_ROTATION = 0.0;
 
-// Ultrasoinc
-#define TRIG_PIN  13
-#define ECHO_PIN  12
-
-SoftwareSerial soft_serial(10, 11); // DYNAMIXELShield UART RX/TX
-#define BLUETOOTH soft_serial
 #define DEBUG_SERIAL Serial
 
 DynamixelShield dxl_shield;
 const float DXL_PROTOCOL_VERSION = 2.0;
-int8_t dxl[3] = {FIRST_DXL, SECOND_DXL, THIRD_DXL};
 
 #define STRING_BUF_NUM 64
 String cmd[STRING_BUF_NUM];
 
-class Ultrasonic
+void dxl_setup(uint8_t id)
 {
- public:
-  Ultrasonic(){}
-  ~Ultrasonic(){}
+  dxl_shield.ping(id);
 
-  init(int set_trig_pin, int set_echo_pin)
-  {
-    trig_pin = set_trig_pin;
-    echo_pin = set_echo_pin;
+  dxl_shield.torqueOff(id);
+  
+  const int8_t EXTENDED_POSITION_CONTROL_MODE = 4;
+  dxl_shield.writeControlTableItem(OPERATING_MODE, id, EXTENDED_POSITION_CONTROL_MODE);
 
-    pinMode(trig_pin, OUTPUT);
-    pinMode(echo_pin, INPUT);
-  }
+  const int8_t TIME_BASED_PROFILE = 4;
+  dxl_shield.writeControlTableItem(DRIVE_MODE, id, TIME_BASED_PROFILE);
+  dxl_shield.writeControlTableItem(HOMING_OFFSET, id, 0);
+  dxl_shield.writeControlTableItem(HOMING_OFFSET, id, -1 * dxl_shield.getPresentPosition(id));
 
-  float get_distance()
-  {
-    float duration, distance;
-    digitalWrite(trig_pin, HIGH);
-    delay(10);
-    digitalWrite(trig_pin, LOW);
-   
-    // echoPin 이 HIGH를 유지한 시간을 저장 한다.
-    duration = pulseIn(echo_pin, HIGH);
-    // HIGH 였을 때 시간(초음파가 보냈다가 다시 들어온 시간)을 가지고 거리를 계산 한다.
-    // 340은 초당 초음파(소리)의 속도, 10000은 밀리세컨드를 세컨드로, 왕복거리이므로 2로 나눠준다.
-    distance = ((float)(340 * duration) / 10000) / 2;
-  
-    return distance;
-  }
-
-  float get_filtered_distance()
-  {
-    return average_filter(get_distance());
-  }
-
- private:
-  float average_filter(const float dist)
-  {
-    const int numReadings = 5;
-    static int readings[numReadings];      // the readings from the analog input
-    static int readIndex = 0;     // the index of the current reading
-    static int total = 0;         // the running total
-    int average = 0;              // the average
-  
-    // subtract the last reading:
-    total = total - readings[readIndex];
-    // read from the sensor:
-    readings[readIndex] = dist;
-    // add the reading to the total:
-    total = total + readings[readIndex];
-    // advance to the next position in the array:
-    readIndex = readIndex + 1;
-  
-    // if we're at the end of the array...
-    if (readIndex >= numReadings) 
-    {
-      // ...wrap around to the beginning:
-      readIndex = 0;
-    }
-  
-    // calculate the average:
-    return average = total / numReadings;
-  }
-  
-  int trig_pin;
-  int echo_pin;
-};
-Ultrasonic ultrasonic;
+  dxl_shield.torqueOn(id);
+}
 
 void setup() 
 {
   DEBUG_SERIAL.begin(115200);
-//  BLUETOOTH.begin(9600);
 //  while(!DEBUG_SERIAL);
 
   dxl_shield.begin(1000000);
   dxl_shield.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
-  dxl_shield.ping(FIRST_DXL);
-  dxl_shield.ping(SECOND_DXL);
-  dxl_shield.ping(THIRD_DXL);
-
-  dxl_shield.torqueOff(FIRST_DXL);
-  dxl_shield.torqueOff(SECOND_DXL);
-  dxl_shield.torqueOff(THIRD_DXL);
-
-  const int8_t EXTENDED_POSITION_CONTROL_MODE = 4;
-  dxl_shield.writeControlTableItem(OPERATING_MODE, FIRST_DXL, EXTENDED_POSITION_CONTROL_MODE);
-  dxl_shield.writeControlTableItem(OPERATING_MODE, SECOND_DXL, EXTENDED_POSITION_CONTROL_MODE);
-  dxl_shield.writeControlTableItem(OPERATING_MODE, THIRD_DXL, EXTENDED_POSITION_CONTROL_MODE);
-
-  const int8_t TIME_BASED_PROFILE = 4;
-  dxl_shield.writeControlTableItem(DRIVE_MODE, FIRST_DXL, TIME_BASED_PROFILE);
-  dxl_shield.writeControlTableItem(DRIVE_MODE, SECOND_DXL, TIME_BASED_PROFILE);
-  dxl_shield.writeControlTableItem(DRIVE_MODE, THIRD_DXL, TIME_BASED_PROFILE);
-
-  dxl_shield.writeControlTableItem(HOMING_OFFSET, FIRST_DXL, 0);
-  dxl_shield.writeControlTableItem(HOMING_OFFSET, SECOND_DXL, 0);
-  dxl_shield.writeControlTableItem(HOMING_OFFSET, THIRD_DXL, 0);
-
-  dxl_shield.writeControlTableItem(HOMING_OFFSET, FIRST_DXL, -1 * dxl_shield.getPresentPosition(FIRST_DXL));
-  dxl_shield.writeControlTableItem(HOMING_OFFSET, SECOND_DXL, -1 * dxl_shield.getPresentPosition(SECOND_DXL));
-  dxl_shield.writeControlTableItem(HOMING_OFFSET, THIRD_DXL, -1 * dxl_shield.getPresentPosition(THIRD_DXL));
   
-  dxl_shield.torqueOn(FIRST_DXL);
-  dxl_shield.torqueOn(SECOND_DXL);
-  dxl_shield.torqueOn(THIRD_DXL);
-
-  ultrasonic.init(TRIG_PIN, ECHO_PIN);
+  for (uint8_t cnt = 0, id = 1; cnt < DXL_CNT; cnt++, id++)
+  {
+    dxl_setup(id);
+  }
 }
 
 void set_profile(uint8_t id, int32_t move_time = 2000)
@@ -240,21 +154,14 @@ void loop()
   uint32_t move_time = 50; // milliseconds
   if ((millis()-tick) >= move_time)
   {
-//    DEBUG_SERIAL.print(cmd[0].toInt()); DEBUG_SERIAL.print(" ");
-//    DEBUG_SERIAL.print(cmd[1].toInt()); DEBUG_SERIAL.print(" ");
-//    DEBUG_SERIAL.println(cmd[2].toInt());
-//    if (dxl_shield.readControlTableItem(MOVING, FIRST_DXL) &&
-//        dxl_shield.readControlTableItem(MOVING, SECOND_DXL) &&
-//        dxl_shield.readControlTableItem(MOVING, THIRD_DXL))
-//    {
-//      return;
-//    }
-//    else
-//    {
-        move(FIRST_DXL, cmd[0].toInt(), move_time); // miilis
-        move(SECOND_DXL, cmd[1].toInt(), move_time); // miilis 
-        move(THIRD_DXL, cmd[2].toInt(), move_time); // miilis 
-//    }
+    for (uint8_t cnt = 0, id = 1; cnt < DXL_CNT; cnt++, id++)
+    {
+      move(id, cmd[cnt].toInt(), move_time); // miilis
+    }
+//    move(FIRST_DXL, cmd[0].toInt(), move_time); // miilis
+//    move(SECOND_DXL, cmd[1].toInt(), move_time); // miilis 
+//    move(THIRD_DXL, cmd[2].toInt(), move_time); // miilis 
+    
     tick = millis();
   }
 }
